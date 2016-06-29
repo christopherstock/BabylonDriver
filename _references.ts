6f2eac102dcc33cbf07a7798f68fbdfaa9056e6f
Christopher Stock <email@christopherstock.de>
2016-06-29 16:01:32 +0200

    Added stub classes for checkpoint and ground.

--- _references.ts
+++ _references.ts
@@ -16,6 +16,8 @@
 /// <reference path="src/de/mayflower/mfg/MfgDemo.ts" />
 /// <reference path="src/de/mayflower/mfg/MfgCar.ts" />
 /// <reference path="src/de/mayflower/mfg/MfgTree.ts" />
+/// <reference path="src/de/mayflower/mfg/MfgGround.ts" />
+/// <reference path="src/de/mayflower/mfg/MfgCheckpoint.ts" />
 
 /// <reference path="src/de/mayflower/mfg/base/MfgDebug.ts" />
 /// <reference path="src/de/mayflower/mfg/base/MfgSettings.ts" />
