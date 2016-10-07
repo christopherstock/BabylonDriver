8bdd36342fbe0aac4cd3ed144b9712a397bc8c06
Christopher Stock <email@christopherstock.de>
2016-10-07 17:42:49 +0200

    Refactored simple ground collisions ..

--- src/de/mayflower/mfg/g3d/MfgGroundSimple.ts
+++ src/de/mayflower/mfg/g3d/MfgGroundSimple.ts
@@ -14,24 +14,27 @@
             materialGround.emissiveColor = new BABYLON.Color3( 0.2, 0.2, 0.2 );
             materialGround.backFaceCulling = false;
 
+            var x:number      = -25.0;
+            var y:number      = 50.0;
+            var z:number      = -20.0;
+            var width:number  = 10.0;
+            var height:number = 1.0;
+            var depth:number  = 10.0;
+
             var babylonGround:BABYLON.Mesh = MfgGroundSimple.createBox
             (
                 "Ground1",
-                new BABYLON.Vector3( -25.0, 50.0, -20.0 ),
-                10.0,
-                1.0,
-                10.0,
+                new BABYLON.Vector3( x, y, z ),
+                width,
+                height,
+                depth,
                 new BABYLON.Vector3( 0.0, 0.0, 0.0 ),
                 0.0,
                 materialGround,
                 MfgInit.app.mfgScene.scene
             );
 
-            var boundingBox = babylonGround.getBoundingInfo().boundingBox;
-            var minVec3 = boundingBox.minimumWorld;
-            var maxVec3 = boundingBox.maximumWorld;
-
-            var cannonBox  = new CANNON.Box( new CANNON.Vec3( 10.0, 10.0, 1.0 ) );
+            var cannonBox  = new CANNON.Box( new CANNON.Vec3( width, depth, height ) );
             var cannonBody = new CANNON.Body(
                 {
                     mass: 0,
@@ -41,49 +44,10 @@
             cannonBody.addShape(cannonBox);
             cannonBody.collisionFilterGroup = MfgWorld.GROUP1;
             cannonBody.collisionFilterMask  = MfgWorld.GROUP2;
-            cannonBody.position.set( -25.0, -20.0, 50.0 );
+            cannonBody.position.set( x, z, y );
 
             MfgInit.app.mfgScene.world.world.addBody( cannonBody );
 
-
-/*
-            var cannonPlane:CANNON.Plane = new CANNON.Plane();
-            var shapeBody:CANNON.Body    = new CANNON.Body( { mass: 30 } );
-            shapeBody.addShape( cannonPlane );
-
-            //var pos = new CANNON.Vec3( -25.0, 50.0, -20.0 );
-            shapeBody.position.set( -25.0, 50.0, -20.0 );
-            //shapeBody.
-            shapeBody.velocity.set( 0, 0, 0 );
-            shapeBody.angularVelocity.set( 0, 0, 0 );
-
-            MfgInit.app.mfgScene.world.world.addBody( shapeBody );
-*/
-
-/*
-            var groundBodyBox:CANNON.Box = new CANNON.Box( new CANNON.Vec3( 100.0, 100.0, 100.0 ) );
-            var groundBody:CANNON.Body   = new CANNON.Body(
-                {
-                    mass:     1.0
-                }
-            );
-            groundBody.addShape( groundBodyBox, new CANNON.Vec3( -50.0, -50.0, -25.0 ));
-
-            MfgInit.app.mfgScene.world.world.addBody( groundBody );
-*/
-
-/*
-            var groundBodyBox:CANNON.Box = new CANNON.Box( new CANNON.Vec3( 100.0, 100.0, 100.0 ) );
-            var groundBody = new CANNON.Body(
-                {
-                    mass:     0
-                }
-            );
-            groundBody.addShape( groundBodyBox );
-            groundBody.position.set( -50.0, -50.0, -25.0 );
-
-            MfgInit.app.mfgScene.world.world.addBody( groundBody );
-*/
             MfgInit.app.onGroundLoaded();
         }
 
